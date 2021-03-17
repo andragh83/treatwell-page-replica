49c893f51279c9a1cce6f2dfcc0b2f7febed8aa5
Andra Ghitulescu <andra.ghitulescu@gmail.com>
2021-03-17 21:20:45 +0000

    adding animations to carousel component and fixing stickycollapse component overlaps

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect, useRef }  from 'react';
 import styles from './Carousel.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
@@ -6,24 +6,40 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 const Carousel = ({ images }) => {
     
     const [position, setPosition] = useState(0);
-    let image = images.filter((image, index) => index === position);
-    return <div className={styles.carouselWrapper}>    
-                    <img className={styles.img} src={image[0]} alt=""/>                                
-                <div className={styles.carouselControls}>
-                    <button className={styles.arrow} onClick={() => {position === 0 ? setPosition(images.length-1) : setPosition(position-1)}}>
-                        <FontAwesomeIcon icon={faChevronLeft} size="2x"/>
-                    </button>                                       
-                    <button className={styles.arrow} onClick={() => {position === images.length-1 ? setPosition(0) : setPosition(position+1)}}>
-                        <FontAwesomeIcon icon={faChevronRight} size="2x"/>
-                    </button>
-                </div>
-                <div style={{marginLeft: '10px'}}>
-                    <div className={styles.bullets}>
-                        {images.map((image, index) => (
-                            index===position ? <div key={index} className={styles.activeBullet}/> : <div key={index} className={styles.bullet}/> 
-                        ))} 
-                    </div>  
-                </div>              
+    const [imageWidth, setImageWidth] = useState(0);
+    const widthRef=useRef();
+
+    useEffect(() => {
+        setImageWidth(widthRef.current.offsetWidth)
+    }, [])
+
+    return <div ref={widthRef} className={styles.carouselWrapper}>
+                <div className={styles.imgGallery}>
+                    <div className={styles.imgContainer}>
+                        {images.map((image, index) => (                            
+                            <div key={index} className={styles.imgWrapper} style={{transform: 'translateX(' + (index-position)*imageWidth+'px)'}}>
+                                <div className={styles.imgItem}>                                    
+                                    <img className={styles.img} src={image} alt=""/>                                    
+                                </div>
+                            </div>                            
+                        ))}                     
+                    </div> 
+                    <div className={styles.carouselControls}>
+                        <button className={styles.arrow} onClick={() => {position === 0 ? setPosition(images.length-1) : setPosition(position-1)}}>
+                            <FontAwesomeIcon icon={faChevronLeft} size="2x"/>
+                        </button>                                       
+                        <button className={styles.arrow} onClick={() => {position === images.length-1 ? setPosition(0) : setPosition(position+1)}}>
+                            <FontAwesomeIcon icon={faChevronRight} size="2x"/>
+                        </button>
+                    </div>
+                    <div className={styles.bulletsContainer}>
+                        <div className={styles.bullets}>
+                            {images.map((image, index) => (
+                                index===position ? <div key={index} className={styles.activeBullet}/> : <div key={index} className={styles.bullet}/> 
+                            ))} 
+                        </div>  
+                    </div>                                                                                 
+                </div>         
             </div>  
 }
 
